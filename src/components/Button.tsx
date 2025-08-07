27c06ba7794d0234dc6330cabc1dce895447eb75
sirhmvfx <sirhmvfx@gmail.com>
2025-08-07 14:16:36 +0100

    modified button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,11 +15,11 @@ function Button({
   href = "", 
   className = "" 
 }: ButtonProps) {
-  const baseStyles = "px-6 py-2  font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 "
+  const baseStyles = "px-6 py-2  font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer transition-all duration-200"
   
   const variantStyles = {
-    filled: "bg-blue-600 text-white hover:bg-blue-700",
-    outline: "border border-white/50 text-white/50 bg-transparent hover:bg-blue-50"
+    filled: "bg-blue-600 text-white hover:bg-blue-700 hover:scale-110 hover:shadow-2xl hover:shadow-blue-500 transition-all duration-200",
+    outline: "border border-white/50 text-white/50 bg-transparent hover:bg-white/10 hover:scale-110 hover:shadow-2xl hover:shadow-white/20 transition-all duration-200"
   }
 
   const buttonClasses = `${baseStyles} ${variantStyles[variant]} ${className}`
