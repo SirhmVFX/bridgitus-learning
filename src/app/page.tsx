e5b9131d6351cdcf262f0a14b803a36ae8e855d5
sirhmvfx <sirhmvfx@gmail.com>
2025-08-07 14:16:20 +0100

    added brief section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Brief from "@/components/Brief";
 import HeroSection from "@/components/Herosection";
 
 export default function Home() {
   return (
     <div>
      <HeroSection />
+     <Brief />
     </div>
   );
 }
