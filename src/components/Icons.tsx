8cfa4aae1c2374321d28052965d336e644e64439
sirhmvfx <sirhmvfx@gmail.com>
2025-08-07 13:44:33 +0100

    added icon component

---
+++ src/components/Icons.tsx
@@ -0,0 +1,8 @@
+const width = 19
+const height = 19
+
+export const ArrowRightIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+    )
+}
