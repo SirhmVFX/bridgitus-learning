48f3254272b5edf237e32ce9ff237a7259600ac2
sirhmvfx <sirhmvfx@gmail.com>
2025-08-09 08:49:37 +0100

    change brand font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {  Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 
 const montserrat = Montserrat({
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
