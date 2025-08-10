0d8ee62b2f8b9616c1a7cdfec86a4b9eeae31da1
sirhmvfx <sirhmvfx@gmail.com>
2025-08-10 08:50:47 +0100

    added new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 import Brief from "@/components/Brief";
 import HeroSection from "@/components/Herosection";
+import { Verified } from "@/components/Icons";
+import Partners from "@/components/Partners";
+import Why from "@/components/Why";
+
 
 export default function Home() {
   return (
     <div>
      <HeroSection />
      <Brief />
+     <Partners />
+     <Why />
+
+     <section className="bg-black">
+      <div className="md:w-[1200px] md:mx-auto grid grid-cols-3 ">
+        <div className="flex items-center gap-2 p-6 bg-black/30">
+         <Verified />
+          <h1 className="text-white">Boost Confidence</h1>
+        </div>
+
+        <div className="flex items-center gap-2 p-6 bg-black/60">
+          <Verified />
+          <h1 className="text-white">Improve Marks</h1>
+        </div>
+
+        <div className="flex items-center gap-2 p-6 bg-black/90">
+          <Verified />
+          <h1 className="text-white">Realise Potentials</h1>
+        </div>
+      </div>
+     </section>
     </div>
   );
 }
