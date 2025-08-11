2c17881071696e16d83f5c129754fbb38fc6b32e
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 16:12:48 +0100

    fix heri

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Button from "./Button";
 import { ArrowRightIcon } from "./Icons";
 
 function HeroSection() {
   return (
-    <section className="bg-black h-screen">
+    <section className="bg-black h-screen relative">
+    <Image src="/assets/line.png" alt="hero" width={1000} height={1000} className="w-full h-full object-cover absolute top-0 left-0 z-0" />
       <div className="md:w-[1200px] md:mx-auto flex justify-between md:items-center py-20 md:py-0 px-4 md:px-0 h-full">
         <div className="md:w-1/2 flex flex-col gap-5">
           <div className="p-1 bg-white/30 flex items-center gap-2 w-fit rounded-full border border-white/20 animate-pulse hover:animate-none hover:border-white/40 transition-all duration-300">
