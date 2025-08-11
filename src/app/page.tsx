69bb6c8d3d201ff512e84179b0c06f35fa20d77a
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 16:11:44 +0100

    added cta offer sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Brief from "@/components/Brief";
+import Cta from "@/components/Cta";
 import HeroSection from "@/components/Herosection";
 import { Verified } from "@/components/Icons";
+import Offer from "@/components/Offer";
 import Partners from "@/components/Partners";
 import Why from "@/components/Why";
 
@@ -17,20 +19,23 @@ export default function Home() {
       <div className="md:w-[1200px] md:mx-auto grid grid-cols-3 ">
         <div className="flex items-center gap-2 p-6 bg-black/30">
          <Verified />
-          <h1 className="text-white">Boost Confidence</h1>
+          <h1 className="text-white md:text-lg text-xs">Boost Confidence</h1>
         </div>
 
         <div className="flex items-center gap-2 p-6 bg-black/60">
           <Verified />
-          <h1 className="text-white">Improve Marks</h1>
+          <h1 className="text-white md:text-lg text-xs">Improve Marks</h1>
         </div>
 
         <div className="flex items-center gap-2 p-6 bg-black/90">
           <Verified />
-          <h1 className="text-white">Realise Potentials</h1>
+          <h1 className="text-white md:text-lg text-xs">Realise Potentials</h1>
         </div>
       </div>
      </section>
+
+     <Offer />
+     <Cta />
     </div>
   );
 }
