8e219d67df06716f5ade55ccb8e65d5e18e5e818
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 08:51:25 +0100

    chnage button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,10 +15,10 @@ function Button({
   href = "", 
   className = "" 
 }: ButtonProps) {
-  const baseStyles = "px-6 py-4  font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer transition-all duration-200"
+  const baseStyles = "px-6 py-4  font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer transition-all duration-200 text-xs"
   
   const variantStyles = {
-    filled: "bg-blue-600 text-white hover:bg-blue-700 hover:scale-110 hover:shadow-2xl hover:shadow-blue-500 transition-all duration-200",
+    filled: "bg-secondary-color text-white hover:bg-blue-700 hover:scale-110 hover:shadow-2xl hover:shadow-blue-500 transition-all duration-200",
     outline: "border border-white/50 text-white/50 bg-transparent hover:bg-white/10 hover:scale-110 hover:shadow-2xl hover:shadow-white/20 transition-all duration-200"
   }
 
