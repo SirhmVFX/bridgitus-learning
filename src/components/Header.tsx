05c097dabcd9d63105f01f0d82c0096a359176f7
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 08:51:54 +0100

    added another header for contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,75 @@
-import Link from "next/link"
-import Button from "./Button"
+import Link from "next/link";
+import Button from "./Button";
+import Image from "next/image";
+import { Mail, Menu, Phone, Star } from "./Icons";
 
 function Header() {
   return (
     <header className="absolute top-0 left-0 right-0 z-50 text-white">
-        <div className="w-[1200px] mx-auto flex justify-between py-5">
-      <h1>Logo</h1>
+      <div className="hidden md:block py-3 bg-white/5">
+        <div className="w-[1200px] mx-auto flex items-center justify-between ">
+          <div className="flex items-center gap-5 ">
+            <div className="flex items-center gap-2">
+              <Phone />
+              <Link href="tel:+2348123456789" className="text-xs">
+                +234 812 345 6789
+              </Link>
+            </div>
 
-      <div>
-        <ul className="flex gap-5 items-center">
-            <Link className="text-sm" href="/about">About</Link>
-            <Link className="text-sm" href="/projects">Projects</Link>
-            <Link className="text-sm" href="/training">Training</Link>
-            <Link className="text-sm" href="/community">Community</Link>
-            <Link className="text-sm" href="/contact">Contact</Link>
-            <Button type="button">Register Now</Button>
-        </ul>
+            <div className="flex items-center gap-2">
+              <Mail />
+              <Link href="mailto:info@bridgitus.com" className="text-xs">
+                info@bridgitus.com
+              </Link>
+            </div>
+           
+           
+          </div>
+              <Link href="#" className="text-xs flex items-center gap-2">
+                <Star />5 star rating from 5000+ verified reviews
+              </Link>
+        </div>
+
+        <div></div>
       </div>
-    </div>
+      <div className="w-[1200px] mx-auto flex justify-between py-5 px-4 md:px-0">
+        <div className="md:w-[350px] w-[250px]">
+          <Image
+            width={1000}
+            height={1000}
+            src="/assets/logow.png"
+            alt="logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
+
+        <div className="hidden md:block">
+          <ul className="flex gap-5 items-center">
+            <Link className="text-xs" href="/about">
+              Why Bridgitus
+            </Link>
+            <Link className="text-xs" href="/projects">
+              Projects
+            </Link>
+            <Link className="text-xs" href="/training">
+              Training
+            </Link>
+            <Link className="text-xs" href="/community">
+              Community
+            </Link>
+            <Link className="text-xs" href="/contact">
+              Contact
+            </Link>
+            <Button type="button">Register Now</Button>
+          </ul>
+        </div>
+
+        <div className="md:hidden block">
+          <Menu />
+        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
