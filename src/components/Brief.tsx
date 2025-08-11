e0b45280009eef6c4d27807d503ac0f4db0aaf83
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 16:12:17 +0100

    made brief mobile reponsive

--- src/components/Brief.tsx
+++ src/components/Brief.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 function Brief() {
   return (
-    <section className="bg-gradient-to-b from-black to-black/20 md:h-[calc(100vh-200px)] flex items-center">
+    <section className="bg-gradient-to-b from-black to-black/20 md:h-[calc(100vh-200px)] h-fit flex items-center md:pb-0 pb-36">
       <div className="md:w-[1200px] md:mx-auto flex md:flex-row flex-col-reverse justify-between gap-20 items-center px-4 md:px-0">
         <div className="md:w-1/2 flex flex-col md:gap-5 gap-3 items-start">
           <h1 className="text-white md:text-5xl text-4xl">Bridgitus</h1>
@@ -25,17 +25,16 @@ function Brief() {
         </div>
 
         <div className="md:w-1/2 flex flex-col gap-8 items-start">
-          <h1 className="text-white md:text-6xl text-4xl italic">
-            “Our services are designed to help you know more, and do more, so
-            you can be more.”
+          <h1 className="text-white md:text-5xl text-3xl italic">
+            “Let your passion for learning be louder than your doubts, your dreams brighter than your fears, and your determinations stronger than your excuses.”
           </h1>
           <div className="flex md:gap-5 gap-2 items-center">
-            <div className="w-12 h-12 ">
-              <Image width={1000} height={1000} src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="founder" className="w-full h-full object-cover" />
+            <div className="w-12 h-14 ">
+              <Image width={1000} height={1000} src="/assets/phemmie.png" alt="founder" className="w-full h-full object-cover" />
             </div>
             <div>
-              <h1 className="text-white md:text-2xl text-xl">Mr. John Doe</h1>
-              <p className="text-white/50 md:text-sm text-xs">Founder, Bridgitus</p>
+              <h1 className="text-white md:text-2xl text-xl">Phemmie Olugbogi</h1>
+              <p className="text-white/50 md:text-sm text-xs">Founder, Bridgitus Learning</p>
             </div>
           </div>
         </div>
