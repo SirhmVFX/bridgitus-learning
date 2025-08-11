8e8c9db2368b8c0d9875eaf437b93f34927cda32
sirhmvfx <sirhmvfx@gmail.com>
2025-08-11 08:52:39 +0100

    mod hero section for responsivness

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -4,17 +4,17 @@ import { ArrowRightIcon } from "./Icons";
 function HeroSection() {
   return (
     <section className="bg-black h-screen">
-      <div className="w-[1200px] mx-auto flex justify-between items-center h-full">
-        <div className="w-1/2 flex flex-col gap-5">
+      <div className="md:w-[1200px] md:mx-auto flex justify-between md:items-center py-20 md:py-0 px-4 md:px-0 h-full">
+        <div className="md:w-1/2 flex flex-col gap-5">
           <div className="p-1 bg-white/30 flex items-center gap-2 w-fit rounded-full border border-white/20 animate-pulse hover:animate-none hover:border-white/40 transition-all duration-300">
             <div className="bg-white/30 rounded-full px-2 py-1">
-              <p className="text-xs text-white">🎉 New</p>
+              <p className="text-[8px] md:text-xs text-white">🎉 New</p>
             </div>
-            <h1 className="text-white text-sm">Bridgiton is tailoring tutoring for students who are ready to thrive. 🔥</h1>
+            <h1 className="text-white md:text-sm text-[8px]">Bridgiton is tailoring tutoring for students who are ready to thrive. 🔥</h1>
             <ArrowRightIcon />
           </div>
 
-          <h1 className="text-white text-7xl">
+          <h1 className="text-white md:text-6xl text-5xl">
             Bridging Curiosity and Confidence — One Student at a Time
           </h1>
           <p className="text-white/50 text-lg">
@@ -28,7 +28,7 @@ function HeroSection() {
             
           </div>
         </div>
-        <div></div>
+        
       </div>
     </section>
   );
