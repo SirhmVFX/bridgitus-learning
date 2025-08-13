aac60f4ea2489229aad163ae0c8f4e21c83cc16f
sirhmvfx <sirhmvfx@gmail.com>
2025-08-13 15:28:55 +0100

    added services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,36 @@
+import Cta from "@/components/Cta";
+import { Verified } from "@/components/Icons";
+import Offer from "@/components/Offer";
+import Why from "@/components/Why";
+
+function Services() {
+  return (
+    <div className="md:pt-32">
+      <Why />
+      <section className="bg-black">
+        <div className="md:w-[1000px] md:mx-auto grid grid-cols-3 ">
+          <div className="flex items-center gap-2 p-6 bg-black/30">
+            <Verified />
+            <h1 className="text-white md:text-lg text-xs">Boost Confidence</h1>
+          </div>
+
+          <div className="flex items-center gap-2 p-6 bg-black/60">
+            <Verified />
+            <h1 className="text-white md:text-lg text-xs">Improve Marks</h1>
+          </div>
+
+          <div className="flex items-center gap-2 p-6 bg-black/90">
+            <Verified />
+            <h1 className="text-white md:text-lg text-xs">
+              Realise Potentials
+            </h1>
+          </div>
+        </div>
+      </section>
+      <Offer />
+      <Cta />
+    </div>
+  );
+}
+
+export default Services;
