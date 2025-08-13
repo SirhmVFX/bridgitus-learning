baff05d948c00f4c4fcd050cd818575bc5a82f63
sirhmvfx <sirhmvfx@gmail.com>
2025-08-13 15:28:09 +0100

    refactor landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,30 +12,6 @@ export default function Home() {
       <HeroSection />
       <Brief />
       <Partners />
-      <Why />
-
-      <section className="bg-black">
-        <div className="md:w-[1000px] md:mx-auto grid grid-cols-3 ">
-          <div className="flex items-center gap-2 p-6 bg-black/30">
-            <Verified />
-            <h1 className="text-white md:text-lg text-xs">Boost Confidence</h1>
-          </div>
-
-          <div className="flex items-center gap-2 p-6 bg-black/60">
-            <Verified />
-            <h1 className="text-white md:text-lg text-xs">Improve Marks</h1>
-          </div>
-
-          <div className="flex items-center gap-2 p-6 bg-black/90">
-            <Verified />
-            <h1 className="text-white md:text-lg text-xs">
-              Realise Potentials
-            </h1>
-          </div>
-        </div>
-      </section>
-
-      <Offer />
       <Cta />
     </div>
   );
