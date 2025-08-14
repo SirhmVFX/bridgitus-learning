425da91000aa88a71bb6be1ff5fd5cdb7b1d9edf
sirhmvfx <sirhmvfx@gmail.com>
2025-08-14 13:16:33 +0100

    fixed landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Brief from "@/components/Brief";
 import Cta from "@/components/Cta";
 import HeroSection from "@/components/Herosection";
-import { Verified } from "@/components/Icons";
-import Offer from "@/components/Offer";
 import Partners from "@/components/Partners";
-import Why from "@/components/Why";
 
 export default function Home() {
   return (
