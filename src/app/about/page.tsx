7c7335535c69a211f0c4f78245fea2dbc08b8217
sirhmvfx <sirhmvfx@gmail.com>
2025-08-15 00:40:07 +0100

    fix break point for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ function About() {
             className="w-full h-full object-cover "
           />
         </div>
-        <div className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 absolute top-0 left-0 right-0 bottom-0 flex items-center z-50">
+        <div className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 absolute top-0 left-0 right-0 bottom-0 flex items-center z-50">
           <div className="md:w-2/3 flex flex-col gap-4">
             <h1 className="md:text-5xl text-5xl text-white font-bold">
               Where Every{" "}
@@ -28,15 +28,15 @@ function About() {
               </span>{" "}
               for Success.
             </h1>
-            <p className="text-white">
+            {/* <p className="text-white">
               Leading tech giants to take control of their firm’s entire
               operations through Bridgitus&apos;s dedicated DevOps package.
-            </p>
+            </p> */}
           </div>
         </div>
       </section>
 
-      <section className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 md:py-20 py-10  grid md:grid-cols-1 grid-cols-1 items-center md:gap-14 gap-6">
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 md:py-20 py-10  grid md:grid-cols-1 grid-cols-1 items-center md:gap-14 gap-6">
         <div className=" flex flex-col gap-3  rounded-3xl h-fit ">
           <h1 className="md:text-3xl text-xl font-bold">The Vision</h1>
           <p className="md:text-3xl text-2xl italic text-black/50">
@@ -54,147 +54,7 @@ function About() {
         </div>
       </section>
 
-      <section className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 md:py-0 py-10 md:h-screen flex md:flex-row flex-col items-center md:gap-0 gap-6">
-        <div className="md:w-1/2 flex flex-col gap-6">
-          <h1 className="md:text-3xl text-xl font-bold">
-            Bridgitus Is More than a Company. It&apos;s a Story
-          </h1>
-          <p className="text-sm text-black/50">
-            Bridgitus Learning is a premier online tuition platform dedicated to
-            empowering students with personalized, high-quality education.
-            Founded by a team of passionate educators, we aim to bridge the gap
-            between curiosity and knowledge, helping students achieve their
-            academic goals from the comfort of their homes. Our experienced
-            tutors provide tailored guidance to ensure every learner thrives.
-          </p>
-
-          <h1 className="font-bold">
-            There is no passion to be found playing small — in settling for a
-            life that is less than the one you are capable of living. - Nelson
-            Mandela
-          </h1>
-          <p className="text-sm text-black/50">
-            At our core, we believe every learner deserves an education tailored
-            to their unique needs. Our online one-on-one tuition platform is
-            dedicated to bridging learning gaps by crafting personalized
-            learning materials that empower students to thrive. We understand
-            that no two learners are alike. That’s why our expert tutors
-            leverage adaptive learning assessment platforms to pinpoint each
-            student’s strengths and challenges precisely.
-          </p>
-        </div>
-        <div className="md:w-1/2 flex items-center justify-center relative">
-          <div className="md:w-[300px] md:h-[400px] w-[250px] h-[300px]">
-            <Image
-              src="/assets/i8.jpg"
-              alt="about"
-              width={1000}
-              height={1000}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </div>
-
-          <div className="w-[330px] h-[80px] bg-primary-color rounded-lg absolute -bottom-5 left-1/2 transform -translate-x-1/2 -z-10"></div>
-        </div>
-      </section>
-
-      <section className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 md:py-0 py-20 ">
-        <div className="flex flex-col gap-4 md:w-3/4">
-          <h1 className="md:text-3xl text-xl font-bold">
-            Our Approach is Different — We’re Here to Close Your Learning Gaps.
-          </h1>
-          <p className="text-sm text-black/50">
-            With our four-step process — assessing, personalizing, teaching, and
-            tracking — we give students the fastest path to academic success.
-          </p>
-        </div>
-
-        <div className="py-10 md:block hidden">
-          <Image
-            src="/assets/process.svg"
-            alt="about"
-            width={1000}
-            height={1000}
-            className=" w-full h-full object-contain"
-          />
-        </div>
-
-        <div className="py-10 md:hidden block">
-          <Image
-            src="/assets/proc2.svg"
-            alt="about"
-            width={1000}
-            height={1000}
-            className=" w-full h-full object-contain"
-          />
-        </div>
-      </section>
-
-      <section className="md:w-[1000px] md:h-screen w-full md:mx-auto md:px-0 px-4 py-10 flex md:flex-row items-center flex-col  md:gap-10 gap-8 ">
-        <div className="md:w-1/3 h-full ">
-          <Image
-            src="/assets/i9.jpg"
-            alt="about"
-            width={1000}
-            height={1000}
-            className="w-full h-full object-cover rounded-3xl"
-          />
-        </div>
-        <div className="md:w-2/3 flex flex-col gap-6">
-          <h1 className="md:text-3xl text-xl font-bold">
-            Today, Tomorrow, or the Future. We Share a Large Vision
-          </h1>
-          <p className="text-sm md:text-base  text-black/50">
-            By integrating engaging word problems and real-life experiences into
-            our lessons, we make learning relevant, dynamic, and meaningful. Our
-            customized resources are designed to spark curiosity, build
-            confidence, and address individual learning gaps effectively.
-          </p>
-
-          <p className="text-sm md:text-base text-black/50">
-            Whether it’s mastering complex concepts, reinforcing foundational
-            skills, or accelerating toward academic goals, we’re committed to
-            guiding every student with tailored support. Our passionate team
-            combines innovative teaching methods, including adaptive technology
-            and practical applications, with a deep dedication to personalized
-            education. Join us on a transformative journey where learning is
-            engaging, impactful, and uniquely yours.
-          </p>
-        </div>
-      </section>
-
-      <section className="md:w-[1000px] md:h-screen md:gap-20 gap-8 w-full md:mx-auto md:px-0 px-4 py-20 flex md:flex-row flex-col items-center ">
-        <div className="md:w-1/2 flex flex-col gap-4">
-          <h1 className="md:text-3xl text-xl font-bold">
-            We Are Changing the Education Narrative: Your Success Is our
-            passion!
-          </h1>
-          <p className="text-sm md:text-base text-black/50">
-            At Bridgitus Learning, we believe education should be more than
-            memorizing facts — it should inspire curiosity, spark confidence,
-            and unlock potential. We’re redefining the way students learn by
-            offering personalized, one-on-one and group tutoring that adapts to
-            each learner’s needs. Our passionate educators go beyond the
-            textbook, blending innovative teaching methods, real-world examples,
-            and adaptive technology to make learning engaging, relevant, and
-            impactful. Every lesson is a step toward mastery, every interaction
-            a chance to empower. Your success isn’t just our goal — it’s the
-            driving force behind everything we do.
-          </p>
-        </div>
-
-        <div className="md:w-1/2 h-full ">
-          <Image
-            src="/assets/i11.jpg"
-            alt="about"
-            width={1000}
-            height={1000}
-            className="w-full h-full object-cover rounded-3xl"
-          />
-        </div>
-      </section>
-
-      <section className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 py-10 flex flex-col gap-10  ">
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 py-10 flex flex-col gap-10  ">
         <h1 className="text-4xl italic">From the directors corner</h1>
         <div className="flex md:flex-row  flex-col  md:gap-10 gap-8">
           <div className="md:w-1/3">
@@ -253,8 +113,147 @@ function About() {
         </div>
       </section>
 
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 md:py-14 py-10 xl:h-screen flex md:flex-row flex-col items-center md:gap-0 gap-6">
+        <div className="md:w-1/2 flex flex-col gap-6">
+          <h1 className="md:text-3xl text-xl font-bold">
+            Bridgitus Is More than a Company. It&apos;s a Story
+          </h1>
+          <p className="text-sm text-black/50">
+            Bridgitus Learning is a premier online tuition platform dedicated to
+            empowering students with personalized, high-quality education.
+            Founded by a team of passionate educators, we aim to bridge the gap
+            between curiosity and knowledge, helping students achieve their
+            academic goals from the comfort of their homes. Our experienced
+            tutors provide tailored guidance to ensure every learner thrives.
+          </p>
+
+          <h1 className="font-bold italic text-lg">
+            &quot;Education is the passport to the future, for tomorrow belongs
+            to those who prepare for it today&quot; - Malcolm X.
+          </h1>
+          <p className="text-sm text-black/50">
+            At our core, we believe every learner deserves an education tailored
+            to their unique needs. Our online one-on-one tuition platform is
+            dedicated to bridging learning gaps by crafting personalized
+            learning materials that empower students to thrive. We understand
+            that no two learners are alike. That’s why our expert tutors
+            leverage adaptive learning assessment platforms to pinpoint each
+            student’s strengths and challenges precisely.
+          </p>
+        </div>
+        <div className="md:w-1/2 flex items-center justify-center relative">
+          <div className="md:w-[300px] md:h-[400px] w-[250px] h-[300px]">
+            <Image
+              src="/assets/i8.jpg"
+              alt="about"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+
+          <div className="w-[330px] h-[80px] bg-primary-color rounded-lg absolute -bottom-5 left-1/2 transform -translate-x-1/2 -z-10"></div>
+        </div>
+      </section>
+
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 md:py-14 py-20 ">
+        <div className="flex flex-col gap-4 md:w-3/4">
+          <h1 className="md:text-3xl text-xl font-bold">
+            Our Approach is Different — We’re Here to Close Your Learning Gaps.
+          </h1>
+          <p className="text-sm text-black/50">
+            With our four-step process — assessing, personalizing, teaching, and
+            tracking — we give students the fastest path to academic success.
+          </p>
+        </div>
+
+        <div className="py-10 md:block hidden">
+          <Image
+            src="/assets/process.svg"
+            alt="about"
+            width={1000}
+            height={1000}
+            className=" w-full h-full object-contain"
+          />
+        </div>
+
+        <div className="py-10 md:hidden block">
+          <Image
+            src="/assets/proc2.svg"
+            alt="about"
+            width={1000}
+            height={1000}
+            className=" w-full h-full object-contain"
+          />
+        </div>
+      </section>
+
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] md:h-screen w-full md:mx-auto md:px-0 px-4 py-10 flex md:flex-row items-center flex-col  md:gap-10 gap-8 ">
+        <div className="md:w-1/3 h-full ">
+          <Image
+            src="/assets/i9.jpg"
+            alt="about"
+            width={1000}
+            height={1000}
+            className="w-full h-full object-cover rounded-3xl"
+          />
+        </div>
+        <div className="md:w-2/3 flex flex-col gap-6">
+          <h1 className="md:text-3xl text-xl font-bold">
+            Today, Tomorrow, or the Future. We Share a Large Vision
+          </h1>
+          <p className="text-sm md:text-base  text-black/50">
+            By integrating engaging word problems and real-life experiences into
+            our lessons, we make learning relevant, dynamic, and meaningful. Our
+            customized resources are designed to spark curiosity, build
+            confidence, and address individual learning gaps effectively.
+          </p>
+
+          <p className="text-sm md:text-base text-black/50">
+            Whether it’s mastering complex concepts, reinforcing foundational
+            skills, or accelerating toward academic goals, we’re committed to
+            guiding every student with tailored support. Our passionate team
+            combines innovative teaching methods, including adaptive technology
+            and practical applications, with a deep dedication to personalized
+            education. Join us on a transformative journey where learning is
+            engaging, impactful, and uniquely yours.
+          </p>
+        </div>
+      </section>
+
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] md:h-screen md:gap-20 gap-8 w-full md:mx-auto md:px-0 px-4 py-20 flex md:flex-row flex-col items-center ">
+        <div className="md:w-1/2 flex flex-col gap-4">
+          <h1 className="md:text-3xl text-xl font-bold">
+            We Are Changing the Education Narrative: Your Success Is our
+            passion!
+          </h1>
+          <p className="text-sm md:text-base text-black/50">
+            At Bridgitus Learning, we believe education should be more than
+            memorizing facts — it should inspire curiosity, spark confidence,
+            and unlock potential. We’re redefining the way students learn by
+            offering personalized, one-on-one and group tutoring that adapts to
+            each learner’s needs. Our passionate educators go beyond the
+            textbook, blending innovative teaching methods, real-world examples,
+            and adaptive technology to make learning engaging, relevant, and
+            impactful. Every lesson is a step toward mastery, every interaction
+            a chance to empower. Your success isn’t just our goal — it’s the
+            driving force behind everything we do.
+          </p>
+        </div>
+
+        <div className="md:w-1/2 h-full ">
+          <Image
+            src="/assets/i11.jpg"
+            alt="about"
+            width={1000}
+            height={1000}
+            className="w-full h-full object-cover rounded-3xl"
+          />
+        </div>
+      </section>
+
       <section>
-        <div className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 py-10 flex flex-col gap-4 ">
+        <div className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 py-10 flex flex-col gap-4 ">
           <h1 className="md:text-3xl text-xl font-bold md:w-3/4">
             At BRIDGITUS, it&apos;s all about building stories that last a
             lifetime.
@@ -269,7 +268,7 @@ function About() {
         <Testimonial />
       </section>
 
-      <section className="md:w-[1000px] w-full md:mx-auto md:px-0 px-4 py-20 ">
+      <section className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full md:mx-auto md:px-0 px-4 py-20 ">
         <div className="flex flex-col gap-4  ">
           <h1 className="md:text-3xl text-xl font-bold md:w-3/4">
             We know you have questions, We also have answers
