34c76e73bfd2f2f306430a1021671367a8f55aad
sirhmvfx <sirhmvfx@gmail.com>
2025-08-15 00:41:40 +0100

    fix break point issues in herosection

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -11,7 +11,7 @@ function HeroSection() {
         height={1000}
         className="w-full h-full object-cover absolute top-0 left-0 z-0"
       />
-      <div className="md:w-[1000px] md:mx-auto flex justify-between md:items-center py-20 md:py-0 px-4 md:px-0 h-full relative">
+      <div className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] md:mx-auto flex justify-between md:items-center py-20 md:py-0 px-4 md:px-0 h-full relative">
         <div className="md:w-2/3 flex flex-col gap-5 z-10 py-16 md:py-0">
           {/* <div className="p-1 bg-white/30 flex items-center gap-2 w-fit rounded-full border border-white/20 animate-pulse hover:animate-none hover:border-white/40 transition-all duration-300">
             <div className="bg-white/30 rounded-full px-2 py-1">
@@ -21,7 +21,7 @@ function HeroSection() {
             <ArrowRightIcon />
           </div> */}
 
-          <h1 className="text-white md:text-5xl text-5xl font-bold">
+          <h1 className="text-white md:text-5xl text-5xl font-bold pt-14">
             Bridging Curiosity and Confidence —{" "}
             <span className="text-yellow-400">One Student at a Time</span>
           </h1>
