749d2c0d198de0eb8e0d938d810ae3ecf86a8057
sirhmvfx <sirhmvfx@gmail.com>
2025-08-15 00:41:29 +0100

    fixed break point issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,11 +9,14 @@ function Header() {
   return (
     <header className={`absolute top-0 left-0 right-0 z-50 `}>
       <div className="hidden md:block py-3 bg-[#161616]">
-        <div className="md:w-[1000px] w-full mx-auto flex items-center justify-between ">
+        <div className=" md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full mx-auto flex items-center justify-between ">
           <div className="flex items-center gap-5 ">
             <div className="flex items-center gap-2">
               <Phone />
-              <Link href="tel:+0433600592" className={`text-xs text-white`}>
+              <Link
+                href="tel:+0433600592"
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] text-white`}
+              >
                 +0433600592
               </Link>
             </div>
@@ -22,7 +25,7 @@ function Header() {
               <Mail />
               <Link
                 href="mailto:info@bridgitus.com"
-                className={`text-xs text-white`}
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] text-white`}
               >
                 info@bridgitus.com
               </Link>
@@ -30,15 +33,15 @@ function Header() {
           </div>
           <Link
             href="#"
-            className={`text-xs flex items-center gap-2 text-white`}
+            className={`md:text-[8px] lg:text-[11px] xl:text-[12px] flex items-center gap-2 text-white`}
           >
             <Star />5 star rating from 5000+ verified reviews
           </Link>
         </div>
       </div>
       <div className="bg-black">
-        <div className="md:w-[1000px] mx-auto flex justify-between items-center py-1 px-4 md:px-0  text-white">
-          <div className="md:w-[270px] w-[200px]">
+        <div className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] w-full mx-auto flex justify-between items-center py-1 px-4 md:px-0  text-white">
+          <div className="md:w-[170px] lg:w-[200px] xl:w-[250px] w-[200px]">
             <Image
               width={1000}
               height={1000}
@@ -50,20 +53,35 @@ function Header() {
 
           <div className="hidden md:block">
             <ul className="flex gap-5 items-center">
-              <Link className={`text-[11px] `} href="/">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/"
+              >
                 Home
               </Link>
-              <Link className={`text-[11px] `} href="/about">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/about"
+              >
                 About Bridgitus Learning
               </Link>
-              <Link className={`text-[11px] `} href="/services">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/services"
+              >
                 Our Services
               </Link>
-              <Link className={`text-[11px] `} href="/classes">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/classes"
+              >
                 Classes
               </Link>
-              <Link className={`text-[11px] `} href="/subjects">
-                Subjects
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/pricing"
+              >
+                Pricing
                 {/* <div>
                   <Link href="/subjects">Maths</Link>
                   <Link href="/subjects">English </Link>
@@ -72,10 +90,16 @@ function Header() {
                   <Link href="/subjects">Biology</Link>
                 </div> */}
               </Link>
-              <Link className={`text-[11px] `} href="/testimonials">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/testimonials"
+              >
                 Testimonials
               </Link>
-              <Link className={`text-[11px] `} href="/contact">
+              <Link
+                className={`md:text-[8px] lg:text-[11px] xl:text-[12px] `}
+                href="/contact"
+              >
                 Contact
               </Link>
               <Button type="link" href="/register">
