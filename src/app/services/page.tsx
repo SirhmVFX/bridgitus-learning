e4100a1614df1c4b1c8f23d66e5ec6c4eb35b5e8
sirhmvfx <sirhmvfx@gmail.com>
2025-08-15 00:40:45 +0100

    fixed break point and remove snapshot from images

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -8,7 +8,7 @@ function Services() {
     <div className="md:pt-32">
       <Why />
       <section className="bg-black">
-        <div className="md:w-[1000px] md:mx-auto grid grid-cols-3 ">
+        <div className="md:w-[800px] lg:w-[1000px] xl:w-[1250px] md:mx-auto grid grid-cols-3 ">
           <div className="flex items-center gap-2 p-6 bg-black/30">
             <Verified />
             <h1 className="text-white md:text-lg text-xs">Boost Confidence</h1>
