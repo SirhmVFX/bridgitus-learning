92d61b85cbb2b856890fcfa6175f18a63c5683d1
sirhmvfx <sirhmvfx@gmail.com>
2025-08-17 18:47:43 +0100

    mode layout

--- src/app/(register)/layout.tsx
+++ src/app/(register)/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import RegisterLayout from "@/components/RegisterLayout";
 import { Open_Sans } from "next/font/google";
 import "../globals.css";
@@ -6,6 +7,12 @@ const open_sans = Open_Sans({
   variable: "--font-open-sans",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Bridgitus",
+  description:
+    "Bridgitus is a leading online tuition platform committed to delivering personalized, high-quality education that empowers students to succeed.",
+};
 
 export default function RootLayout({
   children,
