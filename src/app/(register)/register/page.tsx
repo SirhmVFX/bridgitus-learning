593ca6a1059f12e4b90cd06428c3352772ae30ce
sirhmvfx <sirhmvfx@gmail.com>
2025-08-18 07:18:12 +0100

    working on form

--- src/app/(register)/register/page.tsx
+++ src/app/(register)/register/page.tsx
@@ -13,10 +13,20 @@ function Register() {
     address: "",
   });
 
+  const [openDays, setOpenDays] = useState<Record<number, boolean>>({});
+
+  const toggleDay = (id: number) => {
+    setOpenDays((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   const [step1, setStep1] = useState(true);
   const [step2, setStep2] = useState(false);
   const [step3, setStep3] = useState(false);
   const [step4, setStep4] = useState(false);
+  const [step5, setStep5] = useState(false);
 
   return (
     <div>
@@ -308,12 +318,357 @@ function Register() {
           </div>
         </div>
       ) : step4 ? (
+        <div className="flex flex-col gap-10">
+          <div className="grid grid-cols-3 gap-4">
+            <div>
+              <label htmlFor="" className="text-[12px]  font-medium">
+                One Student
+              </label>
+              <input type="checkbox" />
+            </div>
+
+            <div>
+              <label htmlFor="" className="text-[12px]  font-medium">
+                Two Students
+              </label>
+              <input type="checkbox" />
+            </div>
+
+            <div>
+              <label htmlFor="" className="text-[12px]  font-medium">
+                Three Students
+              </label>
+              <input type="checkbox" />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="md:text-[20px] lg:text-[22px] xl:text-[24px] font-bold">
+              First student details:
+            </h1>
+
+            <div className="grid md:grid-cols-2 gap-4">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-[12px]  font-medium">
+                  Student first name: *
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your first name"
+                  onChange={(e) =>
+                    setRegisterData({
+                      ...registerData,
+                      organizingFor: e.target.value,
+                    })
+                  }
+                  className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-[12px]  font-medium">
+                  Student last name: *
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your last name"
+                  onChange={(e) =>
+                    setRegisterData({
+                      ...registerData,
+                      organizingFor: e.target.value,
+                    })
+                  }
+                  className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-[12px]  font-medium">
+                  What school do they go to?
+                </label>
+                <input
+                  type="tel"
+                  placeholder="Enter phone number"
+                  onChange={(e) =>
+                    setRegisterData({
+                      ...registerData,
+                      organizingFor: e.target.value,
+                    })
+                  }
+                  className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-[12px]  font-medium">
+                  What grade are they in? *
+                </label>
+                <select
+                  onChange={(e) =>
+                    setRegisterData({
+                      ...registerData,
+                      organizingFor: e.target.value,
+                    })
+                  }
+                  className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+                >
+                  <option value="">Pre-K</option>
+                  <option value="">K</option>
+                  <option value="">1</option>
+                  <option value="">2</option>
+                  <option value="">3</option>
+                  <option value="">4</option>
+                  <option value="">5</option>
+                  <option value="">6</option>
+                  <option value="">7</option>
+                  <option value="">8</option>
+                  <option value="">9</option>
+                  <option value="">10</option>
+                  <option value="">11</option>
+                  <option value="">12</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                What subject do they need support in? *
+              </label>
+              <select
+                onChange={(e) =>
+                  setRegisterData({
+                    ...registerData,
+                    organizingFor: e.target.value,
+                  })
+                }
+                className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+              >
+                <option value="">Math</option>
+              </select>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                What is the main result you are looking for? *
+              </label>
+              <select
+                onChange={(e) =>
+                  setRegisterData({
+                    ...registerData,
+                    organizingFor: e.target.value,
+                  })
+                }
+                className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+              >
+                <option value="">Get better at school</option>
+                <option value="">Boost confidence</option>
+                <option value="">Enjoy the learning experience</option>
+                <option value="">A mix of all</option>
+              </select>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                How can we help?
+              </label>
+              <p className="md:text-[10px] lg:text-[12px] xl:text-[13px]">
+                Please provide as much info about your child as you can - why
+                you are seeking a tutor, if and what struggles they have at
+                school and any other details that may be relevant in matching
+                them up with the right tutor.
+              </p>
+              <textarea
+                onChange={(e) =>
+                  setRegisterData({
+                    ...registerData,
+                    organizingFor: e.target.value,
+                  })
+                }
+                className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px h-[200px]"
+              ></textarea>
+            </div>
+
+            <p className="md:text-[10px] lg:text-[12px] xl:text-[13px]">
+              We would love to know more about the student so we can match them
+              with the perfect tutor for them:
+            </p>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                How can we help?
+              </label>
+              <div className="flex gap-2">
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Struggling at school
+                </button>
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Excelling at school
+                </button>
+
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Somewhere in between
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                How would you describe their attitude towards school?
+              </label>
+              <div className="flex gap-2">
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  They love school
+                </button>
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  They dislike school
+                </button>
+
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Somewhere in between
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                How would you describe their mind?
+              </label>
+              <div className="flex gap-2">
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Creative
+                </button>
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Analytical
+                </button>
+
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Somewhere in between
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                How would you describe their personality?
+              </label>
+              <div className="flex gap-2">
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Shy
+                </button>
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Outgoing
+                </button>
+
+                <button className="p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px] bg-black/5">
+                  Somewhere in between
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="" className="text-[12px]  font-medium">
+                What are their 3 favourite things to do (ie. video games,
+                soccer, swimming)
+              </label>
+              <div className="grid md:grid-cols-3 gap-2">
+                <input
+                  type="text"
+                  placeholder="Enter your first name"
+                  onChange={(e) =>
+                    setRegisterData({
+                      ...registerData,
+                      organizingFor: e.target.value,
+                    })
+                  }
+                  className="w-full bg-transparent p-2 rounded-md border border-gray-300 md:text-[10px] lg:text-[12px] xl:text-[13px"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-2">
+            <button
+              type="button"
+              onClick={() => {
+                setStep3(true);
+                setStep4(false);
+              }}
+              className="px-5 py-3  font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer transition-all duration-200 md:text-[8px] lg:text-[11px] xl:text-[12px] bg-black/5 "
+            >
+              Prev
+            </button>
+
+            <Button
+              type="button"
+              onClick={() => {
+                setStep4(false);
+                setStep5(true);
+              }}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+      ) : step5 ? (
         <div>
-          <h1>Step 4</h1>
+          <div>
+            <h1 className="md:text-[20px] lg:text-[22px] xl:text-[24px] font-bold">
+              First student availabilities:
+            </h1>
+            <p className="md:text-[10px] lg:text-[12px] xl:text-[13px]">
+              Please select all the days and times that could work for lessons.
+              The more options you provide, the faster we will be able to
+              organise the right tutor for you!
+            </p>
+          </div>
+
+          {[
+            {
+              id: 1,
+              day: "Monday - Click to view times",
+              times: [
+                { id: 1, time1: "Monday 07:00 AM", time2: "Monday 07:00" },
+                { id: 2, time1: "Monday 07:30 AM", time2: "Monday 07:30" },
+              ],
+            },
+            {
+              id: 2,
+              day: "Tuesday - Click to view times",
+              times: [
+                { id: 1, time1: "Tuesday 07:00 AM", time2: "Tuesday 07:00" },
+                { id: 2, time1: "Tuesday 07:30 AM", time2: "Tuesday 07:30" },
+              ],
+            },
+          ].map((item) => (
+            <div
+              key={item.id}
+              onClick={() => toggleDay(item.id)}
+              className="flex flex-col gap-2"
+            >
+              <h1>{item.day}</h1>
+              {openDays[item.id] && (
+                <div className="mt-2 pl-4 space-y-2">
+                  {item.times.map((time) => (
+                    <div key={time.id} className="flex items-center gap-2">
+                      <input
+                        type="checkbox"
+                        id={`time-${item.id}-${time.id}`}
+                        className="rounded border-gray-300"
+                      />
+                      <label
+                        htmlFor={`time-${item.id}-${time.id}`}
+                        className="text-sm"
+                      >
+                        {time.time1}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       ) : (
         <div>
-          <h1>Step 1</h1>
+          <h1>stepppp</h1>
         </div>
       )}
     </div>
