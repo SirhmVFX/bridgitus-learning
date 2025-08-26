f4d54fb289b4dc8d8e3d30bb69c85d11f88e3118
sirhmvfx <sirhmvfx@gmail.com>
2025-08-26 12:03:58 +0100

    added new icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -480,3 +480,75 @@ export const Dribble = () => {
     </svg>
   );
 };
+
+export const Schedule = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-blue-500 lucide lucide-calendar-check2-icon lucide-calendar-check-2"
+    >
+      <path d="M8 2v4" />
+      <path d="M16 2v4" />
+      <path d="M21 14V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8" />
+      <path d="M3 10h18" />
+      <path d="m16 20 2 2 4-4" />
+    </svg>
+  );
+};
+
+export const Commit = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-green-500 lucide lucide-git-graph-icon lucide-git-graph"
+    >
+      <circle cx="5" cy="6" r="3" />
+      <path d="M5 9v6" />
+      <circle cx="5" cy="18" r="3" />
+      <path d="M12 3v18" />
+      <circle cx="19" cy="6" r="3" />
+      <path d="M16 15.7A9 9 0 0 0 19 9" />
+    </svg>
+  );
+};
+
+export const Trial = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-pink-500 lucide lucide-waypoints-icon lucide-waypoints"
+    >
+      <circle cx="12" cy="4.5" r="2.5" />
+      <path d="m10.2 6.3-3.9 3.9" />
+      <circle cx="4.5" cy="12" r="2.5" />
+      <path d="M7 12h10" />
+      <circle cx="19.5" cy="12" r="2.5" />
+      <path d="m13.8 17.7 3.9-3.9" />
+      <circle cx="12" cy="19.5" r="2.5" />
+    </svg>
+  );
+};
